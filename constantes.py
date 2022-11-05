ad80dc005586b206e773287892ab4d5c9f3d95bc
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-11-04 22:45:27 -0600

    Refactor: Changed code to OOP

---
+++ constantes.py
@@ -0,0 +1,35 @@
+# Constantes
+limite_superior_1 = 496.07
+limite_superior_2 = 4_210.41
+limite_superior_3 = 7_399.42
+limite_superior_4 = 8_601.50
+limite_superior_5 = 10_298.35
+limite_superior_6 = 20_770.29
+limite_superior_7 = 32_736.83
+limite_superior_8 = 62_500
+limite_superior_9 = 83_333.33
+limite_superior_10 = 250_000
+
+cuota_1 = 0
+cuota_2 = 9.52
+cuota_3 = 247.24
+cuota_4 = 594.21
+cuota_5 = 786.54
+cuota_6 = 1_090.61
+cuota_7 = 3_327.42
+cuota_8 = 6_141.95
+cuota_9 = 15_070.90
+cuota_10 = 21_737.57
+cuota_11 = 78_404.23
+
+porcentaje_1 = .0192
+porcentaje_2 = .0640
+porcentaje_3 = .1088
+porcentaje_4 = .1600
+porcentaje_5 = .1792
+porcentaje_6 = .2136
+porcentaje_7 = .2352
+porcentaje_8 = .3000
+porcentaje_9 = .3200
+porcentaje_10 = .34
+porcentaje_11 = .35
--- tasa_isr_pf.py
+++ tasa_isr_pf.py
@@ -1,137 +1,106 @@
+from constantes import *
 
-# Introduccion
-print("\nCalculo de la tasa de impuesto de Persona Fisica")
-print("    - Usando la Tarifa ISR 2021: Anual")
-print("    - Obtenida del Articulo 96 de la Ley ISR \n")
+class TasaIsrPf():
 
-print("Calculo mas conservador:")
-print("    - Sin ingresos excentos.")
-print("    - Sin deducciones autorizadas.\n")
+    def __init__(self):
+        self.introduccion()
+        self.obtener_salario()
+        self.obtener_valores()
+        self.calcular_tasas()
+        self.imprimir_resultado()
+    
+    def introduccion(self):
+        print("\nCalculo de la tasa de impuesto de Persona Fisica")
+        print("    - Usando la Tarifa ISR 2021: Mensual")
+        print("    - Obtenida del Articulo 96 de la Ley ISR \n")
+        print("Calculo mas conservador:")
+        print("    - Sin ingresos excentos.")
+        print("    - Sin deducciones autorizadas.\n")
+        print("Ultima actualizacion: 12 Noviembre 2021 \n\n")
 
-print("Ultima actualizacion: 2 Junio 2021 \n\n")
+    def obtener_salario(self):
+        self.salario_mensual = int(input("¿Cuál es tu salario bruto mensual?: "))
 
-# Constantes
-limite1 = 7735
-limite2 = 65651.07
-limite3 = 115375.90
-limite4 = 134119.41
-limite5 = 160577.65
-limite6 = 323862
-limite7 = 510451
-limite8 = 974535.03
-limite9 = 299380.04
-limite10 = 3898140.12
+    def obtener_valores(self):
+        if self.salario_mensual <= 0.01:
+            print("Error: El salario anual es demasiado bajo")
+            self.limite_inferior = 0
+            self.porcentaje = 0
+            self.cuota_fija = 0
 
-cuota_fija1 = 0
-cuota_fija2 = 148.51
-cuota_fija3 = 3855.14
-cuota_fija4 = 9265.20
-cuota_fija5 = 12264.16
-cuota_fija6 = 17005.47
-cuota_fija7 = 51883.01
-cuota_fija8 = 95768.74
-cuota_fija9 = 234993.95
-cuota_fija10 = 338944.34
-cuota_fija11 = 1222522.76
+        elif self.salario_mensual <= limite_superior_1:
+            self.limite_inferior = 0.01
+            self.porcentaje = porcentaje_1
+            self.cuota_fija = cuota_1
 
-porcentaje_excedente1 = .0192
-porcentaje_excedente2 = .0640
-porcentaje_excedente3 = .1088
-porcentaje_excedente4 = .1600
-porcentaje_excedente5 = .1792
-porcentaje_excedente6 = .2136
-porcentaje_excedente7 = .2352
-porcentaje_excedente8 = .3000
-porcentaje_excedente9 = .3200
-porcentaje_excedente10 = .3400
-porcentaje_excedente11 = 0.3560
+        elif self.salario_mensual <= limite_superior_2:
+            self.limite_inferior = limite_superior_1
+            self.porcentaje = porcentaje_2
+            self.cuota_fija = cuota_2
 
+        elif self.salario_mensual <= limite_superior_3:
+            self.limite_inferior = limite_superior_2
+            self.porcentaje = porcentaje_3
+            self.cuota_fija = cuota_3
 
-# Inputs
-# salario_bruto_anual = float(input("¿Cuál es tu Salario Bruto Mensual?: "))
-salario_bruto_mensual = int(input("¿Cuál es tu Salario Bruto Mensual?: "))
+        elif self.salario_mensual <= limite_superior_4:
+            self.limite_inferior = limite_superior_3
+            self.porcentaje = porcentaje_4
+            self.cuota_fija = cuota_4
 
+        elif self.salario_mensual <= limite_superior_5:
+            self.limite_inferior = limite_superior_4
+            self.porcentaje = porcentaje_5
+            self.cuota_fija = cuota_5
 
-# Procesing
-salario_bruto_anual = salario_bruto_mensual * 12
+        elif self.salario_mensual <= limite_superior_6:
+            self.limite_inferior = limite_superior_5
+            self.porcentaje = porcentaje_6
+            self.cuota_fija = cuota_6
 
-if salario_bruto_anual < 1:
-    print("Error: El salario anual es demasiado bajo")
-    limite_inferior = 0
-    porcentaje_excedente = 0
-    cuota_fija = 0
+        elif self.salario_mensual <= limite_superior_7:
+            self.limite_inferior = limite_superior_6
+            self.porcentaje = porcentaje_7
+            self.cuota_fija = cuota_7
 
-elif salario_bruto_anual <= limite1:
-    limite_inferior = 0
-    porcentaje_excedente = porcentaje_excedente1
-    cuota_fija = cuota_fija1
+        elif self.salario_mensual <= limite_superior_8:
+            self.limite_inferior = limite_superior_7
+            self.porcentaje = porcentaje_8
+            self.cuota_fija = cuota_8
 
-elif salario_bruto_anual <= limite2:
-    limite_inferior = limite1
-    porcentaje_excedente = porcentaje_excedente2
-    cuota_fija = cuota_fija2
+        elif self.salario_mensual <= limite_superior_9:
+            self.limite_inferior = limite_superior_8
+            self.porcentaje = porcentaje_9
+            self.cuota_fija = cuota_9
 
-elif salario_bruto_anual <= limite3:
-    limite_inferior = limite2
-    porcentaje_excedente = porcentaje_excedente3
-    cuota_fija = cuota_fija3
+        elif self.salario_mensual <= limite_superior_10:
+            self.limite_inferior = limite_superior_9
+            self.porcentaje = porcentaje_10
+            self.cuota_fija = cuota_10
+        else:
+            self.limite_inferior = limite_superior_10
+            self.porcentaje = porcentaje_11
+            self.cuota_fija = cuota_11
 
-elif salario_bruto_anual <= limite4:
-    limite_inferior = limite3
-    porcentaje_excedente = porcentaje_excedente4
-    cuota_fija = cuota_fija4
+    def calcular_tasas(self):
+        self.excedente = self.salario_mensual - self.limite_inferior
+        self.impuesto_marginal = self.excedente * self.porcentaje
+        self.isr_causado = self.cuota_fija + self.impuesto_marginal
+        self.tasa_efectiva = self.isr_causado/self.salario_mensual
 
-elif salario_bruto_anual <= limite5:
-    limite_inferior = limite4
-    porcentaje_excedente = porcentaje_excedente5
-    cuota_fija = cuota_fija5
-
-elif salario_bruto_anual <= limite6:
-    limite_inferior = limite5
-    porcentaje_excedente = porcentaje_excedente6
-    cuota_fija = cuota_fija6
-
-elif salario_bruto_anual <= limite7:
-    limite_inferior = limite6
-    porcentaje_excedente = porcentaje_excedente7
-    cuota_fija = cuota_fija7
-
-elif salario_bruto_anual <= limite8:
-    limite_inferior = limite7
-    porcentaje_excedente = porcentaje_excedente8
-    cuota_fija = cuota_fija8
-
-elif salario_bruto_anual <= limite9:
-    limite_inferior = limite8
-    porcentaje_excedente = porcentaje_excedente9
-    cuota_fija = cuota_fija9
-
-elif salario_bruto_anual <= limite10:
-    limite_inferior = limite9
-    porcentaje_excedente = porcentaje_excedente10
-    cuota_fija = cuota_fija10
-
-else:
-    limite_inferior = limite10
-    porcentaje_excedente = porcentaje_excedente11
-    cuota_fija = cuota_fija11
+    def imprimir_resultado(self):
+        print("\nSalario Mensual: " + "$" + "{:,.2f}".format(self.salario_mensual))
+        print("Salario Anual: " + "$" + "{:,.2f}".format(self.salario_mensual))
+        print("Limite inferior: " + "$" + "{:,.2f}".format(self.limite_inferior))
+        print("Cuota Fija: " + "$" + "{:,.2f}".format(self.cuota_fija))
+        print("Tasa a Excedente: " + "{:,.2f}".format(self.porcentaje*100) + "%")
+        print("Excedente: " + "$" + "{:,.2f}".format(self.excedente))
+        print("Impuesto marginal: " + "$" + "{:,.2f}".format(self.impuesto_marginal))
+        print("ISR Causado: " + "$" + "{:,.2f}".format(self.isr_causado))
+        print("Tasa efectiva de impuestos: "
+            + "{:,.2f}".format((self.tasa_efectiva)*100)
+            + "%")
 
 
-# Calculo
-excedente = salario_bruto_anual - limite_inferior
-impuesto_marginal = excedente * porcentaje_excedente
-isr_causado = cuota_fija + impuesto_marginal
-tasa_efectiva = isr_causado/salario_bruto_anual
-
-# Output
-print("\nSalario Mensual: " + "$" + "{:,.2f}".format(salario_bruto_mensual))
-print("Salario Anual: " + "$" + "{:,.2f}".format(salario_bruto_anual))
-print("Limite inferior: " + "$" + "{:,.2f}".format(limite_inferior))
-print("Cuota Fija: " + "$" + "{:,.2f}".format(cuota_fija))
-print("Tasa a Excedente: " + "{:,.2f}".format(porcentaje_excedente*100) + "%")
-print("Excedente: " + "$" + "{:,.2f}".format(excedente))
-print("Impuesto marginal: " + "$" + "{:,.2f}".format(impuesto_marginal))
-print("ISR Causado: " + "$" + "{:,.2f}".format(isr_causado))
-print("Tasa efectiva de impuestos: "
-      + "{:,.2f}".format((isr_causado/salario_bruto_anual)*100)
-      + "%")
+if __name__ == "__main__":
+    oTasaIsr = TasaIsrPf()
